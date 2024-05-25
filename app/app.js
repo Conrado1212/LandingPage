833f7f6605b93364c5895c74aeb479c10ab674b7
Conrado1212 <conrado1212@gmail.com>
2024-05-25 19:42:44 +0200

    menu overview

--- app/app.js
+++ app/app.js
@@ -12,3 +12,40 @@ $(document).ready(function() {
         stickyNav();
     });
 });
+
+
+
+
+var   body = document.querySelector("body"),
+navbar = body.querySelector("#navbar"),
+toggle = body.querySelector(".toggle"),
+modeSw = body.querySelector(".toggle-sw"),
+modeText = body.querySelector(".mode-text");
+main = body.querySelector("#main-doc");
+
+toggle.addEventListener("click", () =>{
+  navbar.classList.toggle("close");
+  main.classList.toggle("toggle-main");
+})
+
+
+modeSw.addEventListener("click", () =>{
+  body.classList.toggle("white");
+  body.classList.contains("white") ? modeText.innerText ="Light Mode" : modeText.innerText ="Dark Mode"
+ })
+
+window.addEventListener('load',function(){
+    $('#navbar li a').click(function () {
+        $('#navbar li a').removeClass('active');
+  
+      $(this).addClass('active');
+    })
+  })
+
+// get the actual pathname:
+var path = location.pathname;
+// filter menu items to find one that has anchor tag with matching href:
+$('#navbar li a').filter(function(){
+    return '/' + $('a', this).attr('href') === path;
+// add class active to the item:
+}).addClass('active');
