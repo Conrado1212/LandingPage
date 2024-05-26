7d97069433ca3d29f4fa105565a8618d4b8c8c53
Conrado1212 <conrado1212@gmail.com>
2024-05-26 18:19:43 +0200

    menu overview

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    var NavY = $('.top-menu').offset().top;
+    var NavY = $('#header').offset().top;
 
     var stickyNav = function(){
         var ScrollY = $(window).scrollTop();
-        ScrollY > NavY ?  $('.top-menu').addClass('sticky') : $('.top-menu').removeClass('sticky');
+        ScrollY > NavY ?  $('#header').addClass('sticky') : $('#header').removeClass('sticky');
     };
 
     stickyNav();
@@ -16,27 +16,27 @@ $(document).ready(function() {
 
 
 
-var   body = document.querySelector("body"),
-navbar = body.querySelector("#navbar"),
-toggle = body.querySelector(".toggle"),
-modeSw = body.querySelector(".toggle-sw"),
-modeText = body.querySelector(".mode-text");
-main = body.querySelector("#main-doc");
+var  body = document.querySelector("body"),
+navbar = document.getElementById("#header"),
+//toggle = body.querySelector(".toggle"),
+modeSw = body.querySelector(".toggle-sw");
+//modeText = body.querySelector(".mode-text");
+//main = body.querySelector("#main-doc");
 
-toggle.addEventListener("click", () =>{
-  navbar.classList.toggle("close");
-  main.classList.toggle("toggle-main");
-})
+// toggle.addEventListener("click", () =>{
+//   navbar.classList.toggle("close");
+//   main.classList.toggle("toggle-main");
+// })
 
 
 modeSw.addEventListener("click", () =>{
   body.classList.toggle("white");
-  body.classList.contains("white") ? modeText.innerText ="Light Mode" : modeText.innerText ="Dark Mode"
+ // body.classList.contains("white") ? modeText.innerText ="Light Mode" : modeText.innerText ="Dark Mode"
  })
 
 window.addEventListener('load',function(){
-    $('#navbar li a').click(function () {
-        $('#navbar li a').removeClass('active');
+    $('.navbar li a').click(function () {
+        $('.navbar li a').removeClass('active');
   
       $(this).addClass('active');
     })
@@ -45,7 +45,7 @@ window.addEventListener('load',function(){
 // get the actual pathname:
 var path = location.pathname;
 // filter menu items to find one that has anchor tag with matching href:
-$('#navbar li a').filter(function(){
+$('.navbar li a').filter(function(){
     return '/' + $('a', this).attr('href') === path;
 // add class active to the item:
 }).addClass('active');
